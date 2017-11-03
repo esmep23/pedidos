21251062829127d047661334ac75b5c06237c390
StefiPulgar <stefipulgar@gmail.com>
2017-11-03 00:00:09 -0500

    conectando a la BD

--- action/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config['host'] = "138.197.154.196";
-$config['database'] = "sistemapedidos";
-$config['username'] = "admin";
-$config['password'] = "bca13fe2ec497b4534ec2435675dc28435e80f779fefba81";
-?>
